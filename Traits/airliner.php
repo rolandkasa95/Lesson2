f786bfb2c900032706bd7dfda21f59284d872c32
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-24 19:55:35 +0300

    New: added a setter for the hasBusinessClass in Airliner

--- Traits/airliner.php
+++ Traits/airliner.php
@@ -5,9 +5,23 @@ trait Airliner{
     public $hasBusinessClass = false;
     public $operator;
 
+    /**
+     * @param $hasFirstClass
+     * @return $this
+     */
     public function setHasFirstClass($hasFirstClass)
     {
         $this->hasFirstClass = $hasFirstClass;
         return $this;
+    }
+
+    /**
+     * @param $hasBusinessClass
+     * @return $this
+     */
+    public function setHasBusinessClass($hasBusinessClass)
+    {
+        $this->hasBusinessClass = $hasBusinessClass;
+        return  $this;
     }
 }
