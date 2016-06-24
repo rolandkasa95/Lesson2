cd626e8b35d9dcc4b846402c7eb7bc944c2595ae
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-24 17:23:50 +0300

    New: added getColorInfo to the Car Class

--- Classes/Car.php
+++ Classes/Car.php
@@ -4,5 +4,16 @@ require_once 'VehicleBase.php';
 
 class Car extends VehicleBase
 {
+    public $colorHex;
+
+    public function getColorInfo(){
+        $info = parent::getColorInfo();
+        if ($this->colorHex){
+            $data['General'] = parent::getColorInfo();
+            $date['Hex Value'] = $this->colorHex;
+            return $data;
+        }
+        return $info;
+    }
 
 }
