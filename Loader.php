d4a3f1d9d278f6bb8bfd40a55fd6172649350f1e
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-24 16:58:40 +0300

    New: added init function for the autoloader

--- Loader.php
+++ Loader.php
@@ -2,5 +2,12 @@
 
 class Loader
 {
-    
+  public static function init(){
+      spl_autoload_extensions('.php');
+      foreach(ObjectFatoryService::getConfig()['autoloaderpaths'] as $path){
+          set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . DIRECTORY_SEPARATOR . $path .DIRECTORY_SEPARATOR);
+      }
+
+      spl_autoload_register();
+  }
 }
