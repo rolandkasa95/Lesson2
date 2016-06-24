03debef27c41de7180845f9027f62e9e87e6b622
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-24 19:30:37 +0300

    New: added __invoke method to the OrederedList Method

--- Classes/OrderedList.php
+++ Classes/OrderedList.php
@@ -2,5 +2,13 @@
 
 class OrderedList
 {
-    
+    public function __invoke($items)
+    {
+        $output = '<ol>';
+        foreach ($items as $item) {
+            $output .= '<li>' . $item . '</li>';
+        }
+        $output .= '</ol>';
+        return $output;
+    }
 }
