6085e9cc084312fc116aa69ae24256b7de5a70f7
rolandkasa95 <rolandkasa95@gmail.com>
2016-06-24 13:31:57 +0300

    New: Added connection function

--- Model/Model.php
+++ Model/Model.php
@@ -2,5 +2,14 @@
 
 class Model
 {
+    public static $pdo;
+
+    public static function connection($config){
+        try {
+            $pdo = new PDO($config['dsn'], $cofig['user'], $config['pass']);
+        }catch (PDOException $e){
+            echo "Fatal error connecting: " . $e->getMessage();
+        }
+    }
 
 }
